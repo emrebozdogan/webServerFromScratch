514f5f1fb4f86af33c638d674766010cb99804f0
emrebozdogan <emrbzdgn@icloud.com>
2025-10-31 14:41:11 +0300

    Update main application entry point

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponses} from "./middleware/logger.js";
-import { middlewareMetricsInc} from "./middleware/hitTracker.js";
+import { middlewareLogResponses} from "./api/middleware/logger.js";
+import { middlewareMetricsInc} from "./api/middleware/hitTracker.js";
 import { handlerLogRequests } from "./api/logRequests.js";
+import { handlerMetrics } from "./api/metrics.js";
 import { config } from "./config.js";
-import { NextFunction } from "express";
 
 const app = express();
 
 const PORT = 8080;
 
-app.use("/app", middlewareMetricsInc);
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerLogRequests);
-app.get("/reset", (req, res) => {
+app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.get("/admin/reset", (req, res) => {
     config.fileserverHits = 0;
     res.status(200).send("OK");
 });
-app.use("/app", express.static("./src/app"));
-app.use(middlewareLogResponses);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
