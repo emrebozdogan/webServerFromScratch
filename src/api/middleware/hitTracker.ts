b42b014d0637cb762caad4e4a9ae3bbada9bd07b
emrebozdogan <emrbzdgn@icloud.com>
2025-11-09 17:30:38 +0300

    Update hitTracker.ts middleware for request tracking

--- src/api/middleware/hitTracker.ts
+++ src/api/middleware/hitTracker.ts
@@ -2,6 +2,6 @@ import { config } from "../../config.js";
 import { Request, Response, NextFunction } from "express";
 
 export function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
-    config.fileserverHits++;
+    config.api.fileServerHits++;
     next();
 };
