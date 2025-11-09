13c29a4e4b1c65790b997fce3e9f1a2b476d6c00
emrebozdogan <emrbzdgn@icloud.com>
2025-11-09 17:31:21 +0300

    Add schema.ts with database table definitions

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
+    email: varchar("email", { length: 256 }).unique().notNull(),
+});
+
+export type NewUser = typeof users.$inferInsert;
+
+export const chirps = pgTable("chirp", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
+    body: varchar("body", { length: 140 }).notNull(),
+    userId: uuid("user_id").references(() => users.id, {onDelete: "cascade"}).notNull(),
+});
+
+export type Chirp = typeof chirps.$inferSelect;
+export type NewChirp = typeof chirps.$inferInsert;
