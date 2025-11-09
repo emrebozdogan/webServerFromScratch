607cbead2b4f86c39851aa243f9bab943e8e8c7f
emrebozdogan <emrbzdgn@icloud.com>
2025-11-09 17:31:25 +0300

    Add chrips.ts with chirp database queries

---
+++ src/db/queries/chrips.ts
@@ -0,0 +1,27 @@
+import { db } from "../index.js";
+import { chirps , NewChirp, Chirp} from "../schema.js";
+import { asc, eq } from "drizzle-orm";
+
+export async function createChirp(chirp: NewChirp) {
+    const [result] = await db
+    .insert(chirps)
+    .values(chirp)
+    .returning();
+    return result;
+}
+
+export async function getAllChirps() {
+    const result = await db
+    .select()
+    .from(chirps)
+    .orderBy(asc(chirps.createdAt));
+    return result;
+}
+
+export async function getChirpByID(chirpId: string) {
+    const [result] = await db
+    .select()
+    .from(chirps)
+    .where(eq(chirps.id, chirpId));
+    return result;
+}
