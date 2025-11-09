c1dcd3af78165372459d26e3eb71e4dadbdfade6
emrebozdogan <emrbzdgn@icloud.com>
2025-11-09 17:30:29 +0300

    Update logRequests.ts with improved request logging

--- src/api/logRequests.ts
+++ src/api/logRequests.ts
@@ -3,5 +3,5 @@ import { config } from "../config.js";
 
 export async function handlerLogRequests(req: Request, res: Response) {
     res.set("Content-Type", "text/plain; charset=utf-8");
-    res.status(200).send(`Hits: ${config.fileserverHits}`);
+    res.status(200).send(`Hits: ${config.api.fileServerHits}`);
 };
