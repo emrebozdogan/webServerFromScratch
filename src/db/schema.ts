d57be1c651bad33138846c61c2d92f0b58ab719b
emrebozdogan <emrbzdgn@icloud.com>
2025-11-18 22:02:13 +0300

    Update database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,12 @@
-import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
     email: varchar("email", { length: 256 }).unique().notNull(),
+    hashedPassword: varchar("hashed_password", { length: 256}).default("unset").notNull(),
+    isChirpyRed: boolean().default(false).notNull(),
 });
 
 export type NewUser = typeof users.$inferInsert;
@@ -19,3 +21,14 @@ export const chirps = pgTable("chirp", {
 
 export type Chirp = typeof chirps.$inferSelect;
 export type NewChirp = typeof chirps.$inferInsert;
+
+export const refreshTokens = pgTable("refresh_tokens", {
+    token: varchar("token", { length: 256 }).primaryKey(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
+    userId: uuid("user_id").references(() => users.id, {onDelete: "cascade"}).notNull(),
+    expiresAt: timestamp("expires_at").notNull().defaultNow(),
+    revokedAt: timestamp("revoked_at")
+});
+
+export type NewToken = typeof refreshTokens.$inferInsert;
