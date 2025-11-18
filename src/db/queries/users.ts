c6865218f617dffd86af96b5fe990ff7a48e297c
emrebozdogan <emrbzdgn@icloud.com>
2025-11-18 22:02:22 +0300

    Update users database queries

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,5 +1,6 @@
 import { db } from "../index.js";
-import { NewUser, users } from "../schema.js";
+import { NewUser, refreshTokens, users } from "../schema.js";
+import { eq } from "drizzle-orm";
 
 export async function createUser(user: NewUser) {
     const [result] = await db
@@ -12,4 +13,39 @@ export async function createUser(user: NewUser) {
 
 export async function deleteAllUsers() {
     await db.delete(users);
+}
+
+export async function findUserByEmail(email: string) {
+    const [result] = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email));
+    return result;
+}
+
+
+export async function changeUserEmailAndPassword(email: string, hashedNewPassword: string, userID: string) {
+    const rows = await db
+    .update(users)
+    .set({ email: email, hashedPassword: hashedNewPassword})
+    .where(eq(users.id, userID))
+    .returning({
+        id: users.id,
+        email: users.email,
+        createdAt: users.createdAt,
+    });
+
+    return rows[0];
+}
+
+export async function upgradeUser(userID: string) {
+    await db
+    .update(users)
+    .set({ isChirpyRed: true })
+    .where(eq(users.id, userID))
+    .returning({
+        id: users.id,
+        email: users.email,
+        createdAt: users.createdAt,
+    });
 }
